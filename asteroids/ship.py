ab6de9f6263b439a56d8abc5eb3938715c2719e9
Jackkell <bronzeeagle1@hotmail.com>
2017-12-01 03:56:00 -0800

    Remove magic numbers for ship movement

--- asteroids/ship.py
+++ asteroids/ship.py
@@ -11,25 +11,29 @@ from pygame.surface import Surface
 from asteroids.basegameobject import BaseGameObject
 from asteroids.colors import Colors
 from asteroids.meter import Meter
-from asteroids.utility import getPointFromPolarCoordinate, drawArrow, getMagnitude
+from asteroids.utility import getPointFromPolarCoordinate
 
 
 class Ship(BaseGameObject):
-    def __init__(self, position: Tuple[int, int], color: Colors, size: int = 50):
+    def __init__(self, position: Tuple[int, int], color: Colors = Colors.BLUE500, size: int = 50, maxSpeed: float = 500,
+                 maxAcceleration: float = 100, maxRotationSpeed: float = 360, maxRotationalAcceleration: float = 360,
+                 thrustSize: float= 50, thrustColor: Colors = Colors.RED500):
         super().__init__(position)
         self.color: Colors = color
-        self.velocity: array = array([0, 0])
-        self.maxSpeed: float = 500
+        self.thrustColor: Colors = thrustColor
+        self.size: float = size
+        self.thrustSize: float = thrustSize
+        self.maxSpeed: float = maxSpeed
+        self.maxAcceleration: float = maxAcceleration
+        self.maxRotationalSpeed: float = maxRotationSpeed
+        self.maxRotationalAcceleration: float = maxRotationalAcceleration
         self.acceleration: float = 0
-        self.maxAcceleration: float = 100
         self.rotation: float = 0
         self.rotationalVelocity: float = 0
-        self.maxRotationalVelocity: float = 360
         self.rotationalAcceleration: float = 0
-        self.maxRotationalAcceleration: float = 360
-        self.size: float = size
+        self.velocity: array = array([0, 0])
         self.rotationVelocityMeter: Meter = Meter((20, 20), (500, 19), Colors.GRAY50.value, Colors.RED500.value,
-                                                  -1 * self.maxRotationalVelocity, self.maxRotationalVelocity,
+                                                  -1 * self.maxRotationalSpeed, self.maxRotationalSpeed,
                                                   self.rotationalVelocity)
         self.speedMeter: Meter = Meter((20, 40), (500, 19), Colors.GRAY50.value, Colors.WHITE.value, 0, self.maxSpeed,
                                        self.speed)
@@ -66,8 +70,8 @@ class Ship(BaseGameObject):
     def rotationallyAccelerate(self, deltaRotationalVelocity: float) -> None:
         self.rotationalVelocity = self.rotationalVelocity + deltaRotationalVelocity
         self.rotationalVelocity = numpy.clip(self.rotationalVelocity,
-                                             self.maxRotationalVelocity * -1,
-                                             self.maxRotationalVelocity)
+                                             self.maxRotationalSpeed * -1,
+                                             self.maxRotationalSpeed)
 
     def _updateVelocity(self, deltaTime: float) -> None:
         deltaVelocity: float = self.acceleration * deltaTime
@@ -86,6 +90,7 @@ class Ship(BaseGameObject):
         self.rotationallyAccelerate(deltaRotationalVelocity)
 
     def update(self, deltaTime: float) -> None:
+        super().update(deltaTime)
         # TODO: pass keys down instead of getting them here
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
@@ -114,17 +119,16 @@ class Ship(BaseGameObject):
         draw.polygon(surface, self.color, self.shipPoints)
 
     def _drawFire(self, surface: Surface) -> None:
-        backOfShip = getPointFromPolarCoordinate(self.position, self.size // 4, math.radians(180 + self.rotation))
-        fireSize = self.size
+        backOfShip = getPointFromPolarCoordinate(self.position, self.size / 4, math.radians(180 + self.rotation))
         points: List = [
             backOfShip,
-            getPointFromPolarCoordinate(backOfShip, fireSize, math.radians(145 + self.rotation)),
-            getPointFromPolarCoordinate(backOfShip, fireSize // 2, math.radians(165 + self.rotation)),
-            getPointFromPolarCoordinate(backOfShip, fireSize, math.radians(180 + self.rotation)),
-            getPointFromPolarCoordinate(backOfShip, fireSize // 2, math.radians(195 + self.rotation)),
-            getPointFromPolarCoordinate(backOfShip, fireSize, math.radians(215 + self.rotation)),
+            getPointFromPolarCoordinate(backOfShip, self.thrustSize * 0.75, math.radians(145 + self.rotation)),
+            getPointFromPolarCoordinate(backOfShip, self.thrustSize / 2, math.radians(165 + self.rotation)),
+            getPointFromPolarCoordinate(backOfShip, self.thrustSize, math.radians(180 + self.rotation)),
+            getPointFromPolarCoordinate(backOfShip, self.thrustSize / 2, math.radians(195 + self.rotation)),
+            getPointFromPolarCoordinate(backOfShip, self.thrustSize * 0.75, math.radians(215 + self.rotation)),
         ]
-        draw.polygon(surface, Colors.RED500.value, points)
+        draw.polygon(surface, self.thrustColor.value, points)
 
     def draw(self, surface: Surface) -> None:
         super().draw(surface)
@@ -133,4 +137,3 @@ class Ship(BaseGameObject):
         self._drawShip(surface)
         self.rotationVelocityMeter.draw(surface)
         self.speedMeter.draw(surface)
-
